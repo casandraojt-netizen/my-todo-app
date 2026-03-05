2244e0a76788109e7a2e8521ac21b53d3f68fe43
Kent Casandra <you@example.com>
2026-03-06 07:53:26 +0800

    Sort todos by priority

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -1,7 +1,7 @@
 import pool from '../../../lib/db';
 
 export async function GET() {
-    const sql = 'SELECT * FROM todos WHERE done = false OR completed_at >= DATE_TRUNC(\'day\', NOW()) ORDER BY created_at DESC';
+    const sql = 'SELECT * FROM todos WHERE done = false OR completed_at >= DATE_TRUNC(\'day\', NOW()) ORDER BY CASE priority WHEN \'high\' THEN 1 WHEN \'medium\' THEN 2 ELSE 3 END, created_at DESC';
     const result = await pool.query(sql);
     return Response.json(result.rows);
 }
